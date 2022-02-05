c6956930a73050f2478e5073e9ce99be8fa775d1
abhishek11800 <97527067+abhishek11800@users.noreply.github.com>
2022-02-05 15:00:24 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=i;j++)         {             printf("%c ",64+i);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,19 +3,13 @@ int main()
 {
     int i,j,n;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    { 
-        for(int k=n-i;k>0;k--)
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
         {
-        printf(" ");
+            printf("%c ",64+i);
         }
-        for(j=1;j<=n;j++)
-        {
-            if(i==1||i==n||j==1||j==n)
-            printf("*");
-            else
-            printf(" ");
-        }printf("
+        printf("
 ");
     }
 }
